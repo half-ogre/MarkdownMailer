29954e31271464ac5243342289ba3d33729fecc8
Drew Miller <ego@anglicangeek.com>
2011-08-25 23:56:58 -0700

    fixed assembly info; added .nuspec

--- Lib/Properties/AssemblyInfo.cs
+++ Lib/Properties/AssemblyInfo.cs
@@ -1,36 +1,9 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("MarkdownMailer")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("MarkdownMailer")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
+[assembly: AssemblyTitle("AnglicanGeek.MarkdownMailer")]
+[assembly: AssemblyProduct("AnglicanGeek.MarkdownMailer")]
+[assembly: AssemblyCompany("Andrew Miller <ego@anglicangeek.com>")]
+[assembly: AssemblyCopyright("Copyright (C) 2011 Andrew Miller <ego@anglicangeek.com>")]
+[assembly: AssemblyVersion("0.1.0.0")]
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("6593a49f-47e6-460c-b936-41c26cc5acf1")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
